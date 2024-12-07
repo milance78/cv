5c4488c8cfe60b6977229cb23827d7f027f6dbd2
Milan <milance78@yahoo.com>
2024-12-07 09:26:36 +0100

    hashrouter applied

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { CustomizedElementsContextProvider } from './contexts/navigateContext';
